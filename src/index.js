7dcdb6d33698775c2cab9c341d389648bc062c56
HJY <wldud172@gmail.com>
2023-04-05 16:31:10 +0900

    리액트 버전 낮추기: index.js - /client 삭제, root&render 수정. package.json 파일 - material-ui 패키지 추가, react, react-dom 버전 수정.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// react 18.0버전에서 사용하는 root 객체 만들고 렌더링하는 방식 - 하위 버전에서는 동작하지 않음.
+// const root = ReactDOM.createRoot(document.getElementById('root')); 
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
