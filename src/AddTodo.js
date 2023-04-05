5911916ce6e86798e861448709d74055563f4143
HJY <wldud172@gmail.com>
2023-04-06 07:53:32 +0900

    11. React로 목록 추가, 수정, 삭제하기  - 오류 사항 Warning: Failed prop type: Invalid prop `id`/`name` of type `number` supplied to `ForwardRef(InputBase)`, expected `string`. 목록 안 불러와지는 현상. prop type 문제.

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,51 +1,57 @@
-import React, { useState } from "react";
-import { TextField, Paper, Button, Grid } from "@material-ui/core";
+import React from "react";
+import { TextField, Paper, Button, Grid} from "@material-ui/core";
 
-function AddTodo(props) {
-    const [item, setItem] = useState({ title: "" });
-
-    const onInputChange = (e) => {
-        let newItem = {...item}
-        newItem.title = e.target.value;
-        setItem(newItem);
+class AddTodo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {item: {title: ""}};
+        this.add = props.add; // props의 함수를 this.add에 연결, 
+        //props에는 상위 컴포넌트(App.js)의 함수, 매개변수가 들어 있음.
+    }
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item: thisItem});
+        console.log(thisItem);
     }
 
-    const onButtonClick = () => {
-        props.add(item);
-        setItem({ title: "" });// text 값을 추가하고 입력 필드는 초기화시킨다.
+    onButtonClick = () => {
+        this.add(this.state.item);
+        this.setState({item: {title: ""}}); // text 값을 추가하고 입력 필드는 초기화시킨다.
     }
 
-    const enterKeyEventHandler = (e) => {
-        if (e.key == 'Enter') {
-            onButtonClick();
+    enterKeyEventHandler = (e) => {
+        if(e.key == 'Enter') {
+            this.onButtonClick();
         }
     }
-    return (
-        <Paper style={{ margine: 16, padding: 16 }}>
-            <Grid container>
-                <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                    <TextField
-                        placehoder="Add Todo here"
+    render() {
+        return (
+            <Paper style={{margine:16, padding:16}}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{paddingRight:16}}>
+                        <TextField
+                        placeholder="Add Todo here"
                         fullWidth
-                        onChange={onInputChange}
-                        value={item.title}
-                        onKeyPress={enterKeyEventHandler}
-                    />
-                </Grid>
-                <Grid xs={1} md={1} item>
-                    <Button
+                        onChange={this.onInputChange}
+                        value={this.state.item.title}
+                        onKeyPress={this.enterKeyEventHendler}
+                        />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button
                         fullWidth
                         color="secondary"
                         variant="outlined"
-                        onClick={onButtonClick}
-                    >
-                        +
-                    </Button>
+                        onClick={this.onButtonClick}
+                        >
+                            +
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Paper>
-
-    );
+            </Paper>
+        );
+    }
 }
 
 export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -1,57 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Todo from './Todo';
 import AddTodo from './AddTodo';
 import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
 
-function App() {
-  const [items, setItems] = useState([
-    { id: 0, title: "Todo 1 ", done: true },
-    { id: 1, title: "Todo 2 ", done: false },
-  ]);
-  
-  const [todoItems, setTodoItems] = useState("");
-
-  // add 함수 추가
-  const add = (item) => {
-    const thisItems = items;
-    item.id = "ID-" + thisItems.length;//key를 위한 id 추가
-    item.done = false;
-    thisItems.push(item);
-    setItems(thisItems);
-    setTodoItems(items.length > 0 && (
-      <Paper style={{ margin: 16 }}>
-        <List>
-          {items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
-          ))}
-        </List>
-      </Paper>
-    ));
-    console.log(items);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: 0, title: "Todo 1 ", done: true },
+        { id: 1, title: "Todo 2 ", done: false },
+      ],
+    };
   }
 
-  useEffect(() => {
-    setTodoItems(items.length > 0 && (
+  // add 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;   // key를 위한 id 추가
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems }); // update state
+    console.log("items: ", this.state.items);
+  }
+
+  // delete 함수 추가
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items: ", this.state.items);
+    });
+  }
+
+  render() {
+    // todoItems에 this.state.items.length가 0보다 크다면 true이므로 && 뒤에 값을 넘겨준다.
+    // todoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
+    var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
-          {items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete}/>
           ))}
         </List>
       </Paper>
-    ))
-  }, []);
+    );
 
-  return (
-    <div className="App">
-      <Container maxWidth="md">
-        <AddTodo add={add} />
-        <div className="TodoList">{todoItems}</div>
-      </Container>
-    </div>
-  );
-
+    // 생성된 컴포넌트 JSX를 리턴한다.
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
