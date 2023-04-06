60cbdf67b1df76346b02123c8bd0fe2135dacbfa
HJY <wldud172@gmail.com>
2023-04-06 10:55:49 +0900

    14. React + 백엔드 통합(백엔드 작성 후)

--- src/App.js
+++ src/App.js
@@ -1,48 +1,50 @@
 import React from 'react';
 import Todo from './Todo';
-import AddTodo from './AddTodo';
-import { Paper, List, Container } from "@material-ui/core";
+import AddTodo from './AddTodo'
+import {Paper, List, Container} from "@material-ui/core";
 import './App.css';
+import { call } from './service/ApiService';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        { id: 0, title: "Todo 1 ", done: true },
-        { id: 1, title: "Todo 2 ", done: false },
-      ],
+      items : [],
     };
   }
-
   // add 함수 추가
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length;   // key를 위한 id 추가
-    item.done = false;
-    thisItems.push(item);
-    this.setState({ items: thisItems }); // update state
-    console.log("items: ", this.state.items);
+    call("/todo","POST",item).then((response) =>
+      this.setState({items:response.data})
+    );
   }
 
-  // delete 함수 추가
   delete = (item) => {
-    const thisItems = this.state.items;
-    const newItems = thisItems.filter(e => e.id !== item.id);
-    this.setState({ items: newItems }, () => {
-      // 디버깅 콜백
-      console.log("Update Items: ", this.state.items);
-    });
+    call("/todo","DELETE",item).then((response) =>
+      this.setState({items:response.data})
+    );
+  }
+
+  update = (item) => {
+    call("/todo","PUT",item).then((response) =>
+      this.setState({items:response.data})
+    );
+  }
+
+  componentDidMount() {
+    call("/todo","GET", null).then((response) =>
+      this.setState({items:response.data})
+    );
   }
 
   render() {
     // todoItems에 this.state.items.length가 0보다 크다면 true이므로 && 뒤에 값을 넘겨준다.
     // todoItem = this.state.items.length > 0 ? (<Paper></Paper>):""; 이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{ margin: 16 }}>
+      <Paper style={{margin:16}}>
         <List>
-          {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete}/>
+          {this.state.items.map((item,idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} update={this.update} />
           ))}
         </List>
       </Paper>
